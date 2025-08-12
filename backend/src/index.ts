4a0c71a35fd0047883f0deef236658370883b324
Mohit <dhanwaniap@gmail.com>
2025-08-12 14:38:10 +0530

    feat: added prisma client and schemas for user and job postings

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
