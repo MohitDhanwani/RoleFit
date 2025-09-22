c42283d58df39292298dfd1d2805482bf123f269
Mohit <dhanwaniap@gmail.com>
2025-09-22 15:34:29 +0530

    fix: made few changes in user schema and fixed the create user route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,25 +1,55 @@
 import { Router } from "express";
 import { prisma } from "../database/index.js";
+import { UserRoles } from "@prisma/client";
 
-const route: Router = Router();
+const userRoute: Router = Router();
 
-route.post("/create-user", async (req, res) => {
+interface User {
+  username: string;
+  emailAddress: string;
+  contactNumber?: string;
+  role?: UserRoles;
+  isPremiumMember?: boolean;
+  skills?: string[];
+  hasExperience?: boolean;
+  totalMonthsOfExperience?: number;
+  previousJobTitle?: string[];
+  jobsTargetting?: string[];
+}
+
+userRoute.post("/create-user", async (req, res) => {
+  const userdata : User = req.body;
+
   try {
-    const {username, emailAddress} = req.body;
-
-    // Create user in DB
-    const registeredUser = await prisma.user.create({
-      data: {
-       username,
-       emailAddress
+    const checkExistingUser = await prisma.user.findFirst({
+      where: {
+        emailAddress: userdata.emailAddress,
       },
     });
 
-    return res.status(201).json(registeredUser);
-  } catch (error: any) {
-    console.error("Error creating user:", error);
-    return res.status(500).json({ error: "Something went wrong" });
+    if (checkExistingUser == null) {
+      const userDataResponse = await prisma.user.create({
+        data: {
+          username: userdata.username,
+          emailAddress: userdata.emailAddress,
+          contactNumber: userdata.contactNumber ?? null,
+          role: userdata.role ?? UserRoles.USER,
+          isPremiumMember: userdata.isPremiumMember ?? false,
+          skills: userdata.skills ?? [],
+          hasExperience: userdata.hasExperience ?? false,
+          totalMonthsOfExperience: userdata.totalMonthsOfExperience ?? 0,
+          prevJobTitle: userdata.previousJobTitle ?? [],
+          jobsTargetting: userdata.jobsTargetting ?? [],
+        },
+      });
+      return res.status(201).json(userDataResponse);
+    } else {
+      return res.status(409).json("User with this email already exists");
+    }
+  } catch (error) {
+    console.error("Error in creating new user", error);
+    return res.status(400);
   }
 });
 
-export default route;
+export default userRoute;
