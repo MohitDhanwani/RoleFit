36caf9b39806c73895428c959cd522268d0acdc1
Mohit <dhanwaniap@gmail.com>
2025-09-25 19:47:05 +0530

    fix: fixed create user route and added update route along with jwt authentication

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import { prisma } from "../database/index.js";
 import { UserRoles } from "@prisma/client";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+//verify the jwt token when user updates the profile
 
 const userRoute: Router = Router();
 
@@ -13,7 +18,7 @@ interface User {
   skills?: string[];
   hasExperience?: boolean;
   totalMonthsOfExperience?: number;
-  previousJobTitle?: string[];
+  prevJobTitle?: string[];
   jobsTargetting?: string[];
 }
 
@@ -38,18 +43,55 @@ userRoute.post("/create-user", async (req, res) => {
           skills: userdata.skills ?? [],
           hasExperience: userdata.hasExperience ?? false,
           totalMonthsOfExperience: userdata.totalMonthsOfExperience ?? 0,
-          prevJobTitle: userdata.previousJobTitle ?? [],
+          prevJobTitle: userdata.prevJobTitle ?? [],
           jobsTargetting: userdata.jobsTargetting ?? [],
         },
       });
-      return res.status(201).json(userDataResponse);
+      
+      const secretKey = process.env.JWT_SECRET_KEY;
+      if (!secretKey) {
+        return res.status(500).json("JWT secret key is not defined");
+      }
+
+      //assign jwt token
+      const userToken = jwt.sign(userDataResponse.id, secretKey);
+      res.cookie("UserToken", userToken);
+      return res.status(201).json({message: "User created successfully", userToken});
     } else {
       return res.status(409).json("User with this email already exists");
     }
   } catch (error) {
     console.error("Error in creating new user", error);
-    return res.status(400);
+    return res.status(400).json("Error in creating new user");
   }
 });
+
+userRoute.post("/update-user", async (req, res) => {
+  const userDataToUpdate : User = req.body;
+  
+  try {
+    const updatedUserResposne = await prisma.user.update({
+      where:{
+        emailAddress: userDataToUpdate.emailAddress,
+      },
+      data: {
+          username: userDataToUpdate.username,
+          emailAddress: userDataToUpdate.emailAddress,
+          contactNumber: userDataToUpdate.contactNumber ?? null,
+          role: userDataToUpdate.role ?? UserRoles.USER,
+          isPremiumMember: userDataToUpdate.isPremiumMember ?? false,
+          skills: userDataToUpdate.skills ?? [],
+          hasExperience: userDataToUpdate.hasExperience ?? false,
+          totalMonthsOfExperience: userDataToUpdate.totalMonthsOfExperience ?? 0,
+          prevJobTitle: userDataToUpdate.prevJobTitle ?? [],
+          jobsTargetting: userDataToUpdate.jobsTargetting ?? [],
+      }
+    });
+    return res.status(200).json(updatedUserResposne);
+  } catch (error) {
+    console.error("Error in updating user data", error);
+    return res.status(400).json("Error in updating user");
+  }
+})
 
 export default userRoute;
